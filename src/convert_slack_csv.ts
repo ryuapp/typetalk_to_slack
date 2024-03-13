d4da632e724ff84cd1ad68d0daf014832f7a91f9
ryu <114303361+ryuapp@users.noreply.github.com>
2024-03-14 01:01:13 +0900

    🐛 Fix couldn't escape commas

--- src/convert_slack_csv.ts
+++ src/convert_slack_csv.ts
@@ -2,14 +2,20 @@ import { CSV } from "../deps.ts";
 
 /**
  * Attach double quotes to string;
+ * @param {string | number} str
+ * @param {number} commasCount
+ * @returns {string}
  */
-function quoteString(str: string | number) {
-  return `"${str}"`;
+function quoteString(str: string | number, commasCount: number = 1) {
+  for (let i = 0; i < commasCount; i++) {
+    str = `"${str}"`;
+  }
+  return str;
 }
 /**
  * Convert the stream to slack messages in CSV format
  *
- * @param {ReadableStream} csv
+ * @param {string | ReadableStream<string[]>} csv
  * @returns {string} slack messages in CSV format
  */
 export async function convertSlackCSV(csv: string | ReadableStream<string[]>) {
@@ -25,7 +31,7 @@ export async function convertSlackCSV(csv: string | ReadableStream<string[]>) {
     const timestamp = quoteString(new Date(message[5]).getTime() / 1000);
     const channel = quoteString(message[0]);
     const user = quoteString(message[2]);
-    const body = quoteString(message[4].replace(/"/g, '\\"'));
+    const body = quoteString(message[4].replace(/\"/g, '""'));
     messages.push([timestamp, channel, user, body]);
   }
 
